f42050087621efb3ae699c648952a9a4764bc68c
jj_ranalli <jacopo.ranalli@mail.polimi.it>
2023-03-16 16:37:05 +0100

    description fix

--- app/layout/components/DefaultHead/DefaultHead.tsx
+++ app/layout/components/DefaultHead/DefaultHead.tsx
@@ -4,7 +4,7 @@ import { accounts } from "app/layout/components/Social/Social"
 export const appName = "PR-Codex"
 export const appTitle = "PR-Codex"
 export const appDescription =
-  "An AI bot that explains and summarizes PR code diffs, directly on Github. Powered by ChatGPT"
+  "An AI bot that summarizes PR code diffs, directly on Github. Powered by ChatGPT"
 export const appUrl = process.env.NEXT_PUBLIC_APP_URL
 export const twitterAccount = accounts.twitter.split("twitter.com/").pop()
 
