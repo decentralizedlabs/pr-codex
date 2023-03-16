2b5aa372d140e9eacdf23aaab1dc5ee8a2fec758
jj_ranalli <jacopo.ranalli@mail.polimi.it>
2023-03-16 16:42:21 +0100

    meta fix

--- app/head.tsx
+++ app/head.tsx
@@ -16,8 +16,8 @@ export default async function Head() {
       <meta property="og:title" content={appTitle} />
       <meta property="og:description" content={appDescription} />
       <meta property="og:url" content={appUrl} />
-      <meta property="og:image" content={`${appUrl}/og_image.png"`} />
-      <meta property="og:image:alt" content={`${appName} cover image"`} />
+      <meta property="og:image" content={`${appUrl}/og_image.png`} />
+      <meta property="og:image:alt" content={`${appName} cover image`} />
       <meta name="twitter:image" content={`${appUrl}/twitter_card.png`} />
     </>
   )
