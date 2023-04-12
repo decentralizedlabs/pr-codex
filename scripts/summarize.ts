462e3742fc716900cddeda42ba8d4517efb44f82
jj_ranalli <jacopo.ranalli@mail.polimi.it>
2023-04-13 01:51:51 +0200

    refactor: testPayload

--- scripts/summarize.ts
+++ scripts/summarize.ts
@@ -1,20 +1,8 @@
+import { testPayload } from "@utils/github/testPayload"
 import { handleGithubAuth } from "../lib/handleGithubAuth"
 import { summarizePullRequest } from "../lib/summarizePullRequest"
 import dotenv from "dotenv"
 dotenv.config()
-
-const testPayload = {
-  installation: { id: 35293807 },
-  action: "synchronize",
-  pull_request: {
-    number: 4,
-    base: {
-      repo: { owner: { login: "decentralizedlabs" }, name: "pr-codex" },
-      sha: "99dfdd96142d171546b59504fbba84cffe23ebd2"
-    },
-    head: { sha: "51cfb7599b7eea7fd74ec5a0cc4c8c5c77150404" }
-  }
-}
 
 async function main() {
   const octokit = await handleGithubAuth(testPayload)
---
+++ utils/github/testPayload.ts
@@ -0,0 +1,12 @@
+export const testPayload = {
+  installation: { id: 35293807 },
+  action: "synchronize",
+  pull_request: {
+    number: 4,
+    base: {
+      repo: { owner: { login: "decentralizedlabs" }, name: "pr-codex" },
+      sha: "99dfdd96142d171546b59504fbba84cffe23ebd2"
+    },
+    head: { sha: "51cfb7599b7eea7fd74ec5a0cc4c8c5c77150404" }
+  }
+}
