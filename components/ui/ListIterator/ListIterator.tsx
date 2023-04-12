1a6353b9ef5196c728ebb76271b13648a7e92bf5
jj_ranalli <jacopo.ranalli@mail.polimi.it>
2023-04-12 21:12:59 +0200

    fix: import

--- components/ui/ListIterator/ListIterator.tsx
+++ components/ui/ListIterator/ListIterator.tsx
@@ -2,7 +2,7 @@
 
 import { useState } from "react"
 
-export function ListIterator({
+export default function ListIterator({
   initItems,
   wrapperClassname = "grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-3",
   children
