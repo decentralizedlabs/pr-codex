24e7400efe8532676e3f65fbbe21bbfafba4fc12
jj_ranalli <jacopo.ranalli@mail.polimi.it>
2023-04-12 21:10:14 +0200

    refactor: TrustedBy section

--- app/components/Homepage.tsx
+++ app/components/Homepage.tsx
@@ -1,27 +1,8 @@
 import { Button } from "@components/ui"
-import { getInstallations } from "@utils/github/getInstallations"
-import { getRepositories } from "@utils/github/getRepositories"
 import { Footer } from "app/layout/components"
-import Image from "next/image"
-import { githubColors } from "@utils/githubColors"
-import Star from "@components/icons/Star"
-import Fork from "@components/icons/Fork"
+import { TrustedBy } from "./TrustedBy"
 
-export const revalidate = 60 * 60 * 3 // 3 hours
-
-export default async function Homepage() {
-  const installations = await getInstallations()
-  const ids = installations.map((installation) => Number(installation.id))
-  const repositories = await getRepositories(ids)
-  const publicRepos = repositories.filter((repo) => !repo.private)
-  const reposByStars = publicRepos.sort(
-    (a, b) => b.stargazers_count - a.stargazers_count
-  )
-  const topUsersRepos = reposByStars.filter(
-    (repo, index) =>
-      reposByStars.findIndex((r) => r.owner.id === repo.owner.id) === index
-  )
-
+export async function Homepage() {
   return (
     <div className="overflow-hidden">
       <div className="relative isolate pt-14">
@@ -84,93 +65,8 @@ export default async function Homepage() {
             </video>
           </div>
         </div>
-        <div className="pt-24 pb-16 sm:pt-32">
-          <div className="px-6 mx-auto max-w-7xl lg:px-8">
-            {/* <div className="max-w-2xl mx-auto text-center">
-              <h2 className="text-4xl font-bold tracking-tight text-white sm:text-6xl">
-                Trusted by
-              </h2>
-              <div className="flex flex-wrap justify-between mt-16">
-                {topUsersRepos.map((repo) => (
-                  <a
-                    href={repo.owner.html_url}
-                    key={repo.id}
-                    className="flex flex-col items-center justify-center w-1/2 mb-8 sm:w-1/3 md:w-1/4"
-                  >
-                    <Image
-                      src={repo.owner.avatar_url}
-                      alt="Github"
-                      width={100}
-                      height={100}
-                      className="rounded-full"
-                    />
-                    <p className="mt-2 text-sm text-gray-300">
-                      {repo.owner.login}
-                    </p>
-                  </a>
-                ))}
-              </div>
-            </div> */}
-            <div className="max-w-2xl mx-auto">
-              <h2 className="mb-16 text-4xl font-bold tracking-tight text-center text-white sm:text-6xl">
-                Trusted by
-              </h2>
-              <div className="flex flex-wrap justify-between">
-                {topUsersRepos.map((repo) => (
-                  <div className="w-full mb-8 md:w-[48%]" key={repo.id}>
-                    <div className="p-4 border border-gray-700 rounded bg-[#0d1117] h-48 flex flex-col justify-between">
-                      <div>
-                        <div className="flex items-center mb-4">
-                          <Image
-                            src={repo.owner.avatar_url}
-                            alt="Github avatar"
-                            width={40}
-                            height={40}
-                            className="mr-2 rounded-full"
-                          />
-                          <a
-                            className="text-sm font-semibold text-gray-300 hover:text-blue-400"
-                            href={repo.html_url}
-                          >
-                            {repo.full_name}
-                          </a>
-                        </div>
-                        <p className="text-sm text-gray-400 line-clamp-3">
-                          {repo.description}
-                        </p>
-                      </div>
-                      <div className="flex items-center gap-5 mt-3">
-                        <div className="flex items-center">
-                          <div
-                            className="w-4 h-4 mr-2 rounded-full bg-red-50"
-                            style={{
-                              backgroundColor:
-                                githubColors[repo.language]?.color
-                            }}
-                          >
-                            {" "}
-                          </div>
-                          <p className="text-sm text-gray-400">
-                            {repo.language}
-                          </p>
-                        </div>
-                        <p className="flex items-center text-sm text-gray-400">
-                          <Star width="18" height="18" />
-                          <span className="ml-2">{repo.stargazers_count}</span>
-                        </p>
-
-                        <p className="flex items-center text-sm text-gray-400">
-                          <Fork width="18" height="18" />
-                          <span className="ml-2"> {repo.forks_count}</span>
-                        </p>
-                      </div>
-                    </div>
-                  </div>
-                ))}
-              </div>
-            </div>
-          </div>
-        </div>
+        {/* @ts-expect-error Async Server Component */}
+        <TrustedBy />
         <div className="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]">
           <svg
             className="relative left-[calc(50%+3rem)] h-[21.1875rem] max-w-none -translate-x-1/2 sm:left-[calc(50%+36rem)] sm:h-[42.375rem]"
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,8 @@
 export const runtime = "nodejs"
 
-import Homepage from "./components/Homepage"
+import { Homepage } from "./components/Homepage"
 
 export default function Page() {
+  // @ts-expect-error Async Server Component
   return <Homepage />
 }
