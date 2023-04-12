d22ac65cc02f1a16298f1d1ae5987a4196546fc1
jj_ranalli <jacopo.ranalli@mail.polimi.it>
2023-04-12 21:11:16 +0200

    fix: config

--- next.config.js
+++ next.config.js
@@ -1,11 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
-    appDir: true,
-    runtime: "experimental-edge",
-    fontLoaders: [
-      { loader: "@next/font/google", options: { subsets: ["latin"] } }
-    ]
+    appDir: true
   },
   images: {
     remotePatterns: [
