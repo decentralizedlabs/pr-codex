88c7cf4426399ee92e8c2fbbc89c3a17257f9aa9
jj_ranalli <jacopo.ranalli@mail.polimi.it>
2023-04-16 17:41:05 +0200

    feat: add testPayloadComment

---
+++ utils/github/testPayloadComment.ts
@@ -0,0 +1,305 @@
+export const testPayloadComment = {
+  installation: { id: 35293807 },
+  action: "created",
+  issue: {
+    url: "https://api.github.com/repos/decentralizedlabs/pr-codex/issues/4",
+    repository_url: "https://api.github.com/repos/decentralizedlabs/pr-codex",
+    labels_url:
+      "https://api.github.com/repos/decentralizedlabs/pr-codex/issues/4/labels{/name}",
+    comments_url:
+      "https://api.github.com/repos/decentralizedlabs/pr-codex/issues/4/comments",
+    events_url:
+      "https://api.github.com/repos/decentralizedlabs/pr-codex/issues/4/events",
+    html_url: "https://github.com/decentralizedlabs/pr-codex/pull/4",
+    id: 1627438684,
+    node_id: "PR_kwDOJKDDks5MNS9d",
+    number: 4,
+    title: "PR-Codex init",
+    user: {
+      login: "jjranalli",
+      id: 39241410,
+      node_id: "MDQ6VXNlcjM5MjQxNDEw",
+      avatar_url: "https://avatars.githubusercontent.com/u/39241410?v=4",
+      gravatar_id: "",
+      url: "https://api.github.com/users/jjranalli",
+      html_url: "https://github.com/jjranalli",
+      followers_url: "https://api.github.com/users/jjranalli/followers",
+      following_url:
+        "https://api.github.com/users/jjranalli/following{/other_user}",
+      gists_url: "https://api.github.com/users/jjranalli/gists{/gist_id}",
+      starred_url:
+        "https://api.github.com/users/jjranalli/starred{/owner}{/repo}",
+      subscriptions_url: "https://api.github.com/users/jjranalli/subscriptions",
+      organizations_url: "https://api.github.com/users/jjranalli/orgs",
+      repos_url: "https://api.github.com/users/jjranalli/repos",
+      events_url: "https://api.github.com/users/jjranalli/events{/privacy}",
+      received_events_url:
+        "https://api.github.com/users/jjranalli/received_events",
+      type: "User",
+      site_admin: false
+    },
+    labels: [],
+    state: "open",
+    locked: false,
+    assignee: null,
+    assignees: [],
+    milestone: null,
+    comments: 2,
+    created_at: "2023-03-16T13:06:36Z",
+    updated_at: "2023-04-16T15:36:16Z",
+    closed_at: null,
+    author_association: "CONTRIBUTOR",
+    active_lock_reason: null,
+    draft: false,
+    pull_request: {
+      url: "https://api.github.com/repos/decentralizedlabs/pr-codex/pulls/4",
+      html_url: "https://github.com/decentralizedlabs/pr-codex/pull/4",
+      diff_url: "https://github.com/decentralizedlabs/pr-codex/pull/4.diff",
+      patch_url: "https://github.com/decentralizedlabs/pr-codex/pull/4.patch",
+      merged_at: null
+    },
+    body: "<!-- start -->\n\nthis is a test\n\n\n\n<!-- start pr-codex -->\n\n---\n\n## PR-Codex overview\nThis PR adds a new file `route.ts` for handling pull request events on Github, updates the `README.md` file with a test comment, and makes various changes to the UI. The `package-lock.json` file was skipped due to too many changes.\n\n> The following files were skipped due to too many changes: package-lock.json.\n\n<!-- end pr-codex -->",
+    reactions: {
+      url: "https://api.github.com/repos/decentralizedlabs/pr-codex/issues/4/reactions",
+      total_count: 0,
+      "+1": 0,
+      "-1": 0,
+      laugh: 0,
+      hooray: 0,
+      confused: 0,
+      heart: 0,
+      rocket: 0,
+      eyes: 0
+    },
+    timeline_url:
+      "https://api.github.com/repos/decentralizedlabs/pr-codex/issues/4/timeline",
+    performed_via_github_app: null,
+    state_reason: null
+  },
+  comment: {
+    url: "https://api.github.com/repos/decentralizedlabs/pr-codex/issues/comments/1510415128",
+    html_url:
+      "https://github.com/decentralizedlabs/pr-codex/pull/4#issuecomment-1510415128",
+    issue_url:
+      "https://api.github.com/repos/decentralizedlabs/pr-codex/issues/4",
+    id: 1510415128,
+    node_id: "IC_kwDOJKDDks5aBxsY",
+    user: {
+      login: "jjranalli",
+      id: 39241410,
+      node_id: "MDQ6VXNlcjM5MjQxNDEw",
+      avatar_url: "https://avatars.githubusercontent.com/u/39241410?v=4",
+      gravatar_id: "",
+      url: "https://api.github.com/users/jjranalli",
+      html_url: "https://github.com/jjranalli",
+      followers_url: "https://api.github.com/users/jjranalli/followers",
+      following_url:
+        "https://api.github.com/users/jjranalli/following{/other_user}",
+      gists_url: "https://api.github.com/users/jjranalli/gists{/gist_id}",
+      starred_url:
+        "https://api.github.com/users/jjranalli/starred{/owner}{/repo}",
+      subscriptions_url: "https://api.github.com/users/jjranalli/subscriptions",
+      organizations_url: "https://api.github.com/users/jjranalli/orgs",
+      repos_url: "https://api.github.com/users/jjranalli/repos",
+      events_url: "https://api.github.com/users/jjranalli/events{/privacy}",
+      received_events_url:
+        "https://api.github.com/users/jjranalli/received_events",
+      type: "User",
+      site_admin: false
+    },
+    created_at: "2023-04-16T15:36:15Z",
+    updated_at: "2023-04-16T15:36:15Z",
+    author_association: "CONTRIBUTOR",
+    body: "/codex-ask test",
+    reactions: {
+      url: "https://api.github.com/repos/decentralizedlabs/pr-codex/issues/comments/1510415128/reactions",
+      total_count: 0,
+      "+1": 0,
+      "-1": 0,
+      laugh: 0,
+      hooray: 0,
+      confused: 0,
+      heart: 0,
+      rocket: 0,
+      eyes: 0
+    },
+    performed_via_github_app: null
+  },
+  repository: {
+    id: 614515602,
+    node_id: "R_kgDOJKDDkg",
+    name: "pr-codex",
+    full_name: "decentralizedlabs/pr-codex",
+    private: false,
+    owner: {
+      login: "decentralizedlabs",
+      id: 119909906,
+      node_id: "O_kgDOByWuEg",
+      avatar_url: "https://avatars.githubusercontent.com/u/119909906?v=4",
+      gravatar_id: "",
+      url: "https://api.github.com/users/decentralizedlabs",
+      html_url: "https://github.com/decentralizedlabs",
+      followers_url: "https://api.github.com/users/decentralizedlabs/followers",
+      following_url:
+        "https://api.github.com/users/decentralizedlabs/following{/other_user}",
+      gists_url:
+        "https://api.github.com/users/decentralizedlabs/gists{/gist_id}",
+      starred_url:
+        "https://api.github.com/users/decentralizedlabs/starred{/owner}{/repo}",
+      subscriptions_url:
+        "https://api.github.com/users/decentralizedlabs/subscriptions",
+      organizations_url: "https://api.github.com/users/decentralizedlabs/orgs",
+      repos_url: "https://api.github.com/users/decentralizedlabs/repos",
+      events_url:
+        "https://api.github.com/users/decentralizedlabs/events{/privacy}",
+      received_events_url:
+        "https://api.github.com/users/decentralizedlabs/received_events",
+      type: "Organization",
+      site_admin: false
+    },
+    html_url: "https://github.com/decentralizedlabs/pr-codex",
+    description: "A ChatGPT-powered bot to summarize PR code diffs",
+    fork: false,
+    url: "https://api.github.com/repos/decentralizedlabs/pr-codex",
+    forks_url: "https://api.github.com/repos/decentralizedlabs/pr-codex/forks",
+    keys_url:
+      "https://api.github.com/repos/decentralizedlabs/pr-codex/keys{/key_id}",
+    collaborators_url:
+      "https://api.github.com/repos/decentralizedlabs/pr-codex/collaborators{/collaborator}",
+    teams_url: "https://api.github.com/repos/decentralizedlabs/pr-codex/teams",
+    hooks_url: "https://api.github.com/repos/decentralizedlabs/pr-codex/hooks",
+    issue_events_url:
+      "https://api.github.com/repos/decentralizedlabs/pr-codex/issues/events{/number}",
+    events_url:
+      "https://api.github.com/repos/decentralizedlabs/pr-codex/events",
+    assignees_url:
+      "https://api.github.com/repos/decentralizedlabs/pr-codex/assignees{/user}",
+    branches_url:
+      "https://api.github.com/repos/decentralizedlabs/pr-codex/branches{/branch}",
+    tags_url: "https://api.github.com/repos/decentralizedlabs/pr-codex/tags",
+    blobs_url:
+      "https://api.github.com/repos/decentralizedlabs/pr-codex/git/blobs{/sha}",
+    git_tags_url:
+      "https://api.github.com/repos/decentralizedlabs/pr-codex/git/tags{/sha}",
+    git_refs_url:
+      "https://api.github.com/repos/decentralizedlabs/pr-codex/git/refs{/sha}",
+    trees_url:
+      "https://api.github.com/repos/decentralizedlabs/pr-codex/git/trees{/sha}",
+    statuses_url:
+      "https://api.github.com/repos/decentralizedlabs/pr-codex/statuses/{sha}",
+    languages_url:
+      "https://api.github.com/repos/decentralizedlabs/pr-codex/languages",
+    stargazers_url:
+      "https://api.github.com/repos/decentralizedlabs/pr-codex/stargazers",
+    contributors_url:
+      "https://api.github.com/repos/decentralizedlabs/pr-codex/contributors",
+    subscribers_url:
+      "https://api.github.com/repos/decentralizedlabs/pr-codex/subscribers",
+    subscription_url:
+      "https://api.github.com/repos/decentralizedlabs/pr-codex/subscription",
+    commits_url:
+      "https://api.github.com/repos/decentralizedlabs/pr-codex/commits{/sha}",
+    git_commits_url:
+      "https://api.github.com/repos/decentralizedlabs/pr-codex/git/commits{/sha}",
+    comments_url:
+      "https://api.github.com/repos/decentralizedlabs/pr-codex/comments{/number}",
+    issue_comment_url:
+      "https://api.github.com/repos/decentralizedlabs/pr-codex/issues/comments{/number}",
+    contents_url:
+      "https://api.github.com/repos/decentralizedlabs/pr-codex/contents/{+path}",
+    compare_url:
+      "https://api.github.com/repos/decentralizedlabs/pr-codex/compare/{base}...{head}",
+    merges_url:
+      "https://api.github.com/repos/decentralizedlabs/pr-codex/merges",
+    archive_url:
+      "https://api.github.com/repos/decentralizedlabs/pr-codex/{archive_format}{/ref}",
+    downloads_url:
+      "https://api.github.com/repos/decentralizedlabs/pr-codex/downloads",
+    issues_url:
+      "https://api.github.com/repos/decentralizedlabs/pr-codex/issues{/number}",
+    pulls_url:
+      "https://api.github.com/repos/decentralizedlabs/pr-codex/pulls{/number}",
+    milestones_url:
+      "https://api.github.com/repos/decentralizedlabs/pr-codex/milestones{/number}",
+    notifications_url:
+      "https://api.github.com/repos/decentralizedlabs/pr-codex/notifications{?since,all,participating}",
+    labels_url:
+      "https://api.github.com/repos/decentralizedlabs/pr-codex/labels{/name}",
+    releases_url:
+      "https://api.github.com/repos/decentralizedlabs/pr-codex/releases{/id}",
+    deployments_url:
+      "https://api.github.com/repos/decentralizedlabs/pr-codex/deployments",
+    created_at: "2023-03-15T18:38:11Z",
+    updated_at: "2023-04-11T13:51:42Z",
+    pushed_at: "2023-04-16T12:54:04Z",
+    git_url: "git://github.com/decentralizedlabs/pr-codex.git",
+    ssh_url: "git@github.com:decentralizedlabs/pr-codex.git",
+    clone_url: "https://github.com/decentralizedlabs/pr-codex.git",
+    svn_url: "https://github.com/decentralizedlabs/pr-codex",
+    homepage: "https://codex.dlabs.app",
+    size: 17593,
+    stargazers_count: 44,
+    watchers_count: 44,
+    language: "TypeScript",
+    has_issues: true,
+    has_projects: true,
+    has_downloads: true,
+    has_wiki: true,
+    has_pages: false,
+    has_discussions: false,
+    forks_count: 1,
+    mirror_url: null,
+    archived: false,
+    disabled: false,
+    open_issues_count: 1,
+    license: null,
+    allow_forking: true,
+    is_template: false,
+    web_commit_signoff_required: false,
+    topics: ["chatgpt", "github", "github-app", "pull-requests"],
+    visibility: "public",
+    forks: 1,
+    open_issues: 1,
+    watchers: 44,
+    default_branch: "master"
+  },
+  organization: {
+    login: "decentralizedlabs",
+    id: 119909906,
+    node_id: "O_kgDOByWuEg",
+    url: "https://api.github.com/orgs/decentralizedlabs",
+    repos_url: "https://api.github.com/orgs/decentralizedlabs/repos",
+    events_url: "https://api.github.com/orgs/decentralizedlabs/events",
+    hooks_url: "https://api.github.com/orgs/decentralizedlabs/hooks",
+    issues_url: "https://api.github.com/orgs/decentralizedlabs/issues",
+    members_url:
+      "https://api.github.com/orgs/decentralizedlabs/members{/member}",
+    public_members_url:
+      "https://api.github.com/orgs/decentralizedlabs/public_members{/member}",
+    avatar_url: "https://avatars.githubusercontent.com/u/119909906?v=4",
+    description: ""
+  },
+  sender: {
+    login: "jjranalli",
+    id: 39241410,
+    node_id: "MDQ6VXNlcjM5MjQxNDEw",
+    avatar_url: "https://avatars.githubusercontent.com/u/39241410?v=4",
+    gravatar_id: "",
+    url: "https://api.github.com/users/jjranalli",
+    html_url: "https://github.com/jjranalli",
+    followers_url: "https://api.github.com/users/jjranalli/followers",
+    following_url:
+      "https://api.github.com/users/jjranalli/following{/other_user}",
+    gists_url: "https://api.github.com/users/jjranalli/gists{/gist_id}",
+    starred_url:
+      "https://api.github.com/users/jjranalli/starred{/owner}{/repo}",
+    subscriptions_url: "https://api.github.com/users/jjranalli/subscriptions",
+    organizations_url: "https://api.github.com/users/jjranalli/orgs",
+    repos_url: "https://api.github.com/users/jjranalli/repos",
+    events_url: "https://api.github.com/users/jjranalli/events{/privacy}",
+    received_events_url:
+      "https://api.github.com/users/jjranalli/received_events",
+    type: "User",
+    site_admin: false
+  }
+}
