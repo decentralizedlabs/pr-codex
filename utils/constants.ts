b44986f40454409522a419d3b7f02c07e34f67ce
jj_ranalli <jacopo.ranalli@mail.polimi.it>
2023-04-18 13:36:05 +0200

    fix: update skippedFiles and remove maxChanges

--- utils/constants.ts
+++ utils/constants.ts
@@ -1,5 +1,4 @@
 export const startDescription = "\n\n<!-- start pr-codex -->"
 export const endDescription = "<!-- end pr-codex -->"
 export const codexCommand = "/codex "
-export const maxChanges = 200
 export const maxCodeDiff = 11500
--- utils/parseDiff.ts
+++ utils/parseDiff.ts
@@ -1,34 +1,28 @@
-type FileChange = { parsedFiles: string[]; skippedFiles: string[] }
+type FileChange = { parsedFiles: string[] }
 
-export function parseDiff(diff: string, maxChanges: number): FileChange {
-  let skippedFiles: string[] = []
+export function parseDiff(diff: string): FileChange {
   const files = diff.split(/diff --git /).slice(1)
 
-  const parsedFiles = files.flatMap((file) => {
-    const lines = file.split("\n")
+  const parsedFiles = files
+    .map((file) => {
+      const lines = file.split("\n")
 
-    const filepath = lines[0].split(" ")[1]
+      const filepath = lines[0].split(" ")[0].slice(2)
 
-    // Don't consider diff in deleted files
-    if (lines[1].startsWith("deleted")) return `deleted ${filepath}`
+      // Don't consider diff in deleted files
+      if (lines[1].startsWith("deleted")) return `deleted ${filepath}`
 
-    const mainContent = lines.slice(6).flatMap((line) => {
-      if (line.startsWith("+") || line.startsWith("-")) {
-        const trimmedContent = line.slice(1).trim()
-        if (!trimmedContent) return []
-        return line[0] + trimmedContent
-      } else return line.trim()
-    })
-    const changes = mainContent.filter(
-      (line) => line.startsWith("+") || line.startsWith("-")
-    ).length
+      const mainContent = lines.slice(6).flatMap((line) => {
+        if (line.startsWith("+") || line.startsWith("-")) {
+          const trimmedContent = line.slice(1).trim()
+          if (!trimmedContent) return []
+          return line[0] + trimmedContent
+        } else return line.trim()
+      })
 
-    if (changes <= maxChanges) {
       return `${filepath}\n${mainContent.join("\n")}`
-    }
-    skippedFiles.push(`\`${filepath.slice(2)}\``)
-    return []
-  })
+    })
+    .sort((a, b) => a.length - b.length)
 
-  return { parsedFiles, skippedFiles }
+  return { parsedFiles }
 }
