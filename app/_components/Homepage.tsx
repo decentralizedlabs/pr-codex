eaf8c33a3f5e614feab7afee3f876c6e971259af
jj_ranalli <jacopo.ranalli@mail.polimi.it>
2024-09-23 12:11:25 +0200

    fix

--- app/_components/Homepage.tsx
+++ app/_components/Homepage.tsx
@@ -65,6 +65,7 @@ export function Homepage() {
             </video>
           </div>
         </div>
+        {/* @ts-expect-error Async Server Component */}
         <TrustedBy />
         <div className="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]">
           <svg
